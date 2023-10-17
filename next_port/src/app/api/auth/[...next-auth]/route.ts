931939814b0db14cc1203674c96d24fb6a2d1c25
Sama-E <112719147+Sama-E@users.noreply.github.com>
2023-10-17 12:02:51 -0400

    create user

--- next_port/src/app/api/auth/[...next-auth]/route.ts
+++ next_port/src/app/api/auth/[...next-auth]/route.ts
@@ -1,3 +1,4 @@
+import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 const handler = NextAuth({
@@ -15,14 +16,27 @@ const handler = NextAuth({
 				},
 				async authorize(credentials, req) {
 				// Add logic here to look up the user from the credentials supplied
-				const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+				// const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+				const res = await fetch ("/api/login", { 
+					method: "POST",
+					headers:{
+						"Content-Type": "application/json",
+					},
+
+					body: JSON.stringify({
+						username: credentials?.username,
+						password: credentials?.password
+					})
+				});
+
+				const user = await res.json();
 
 				if (user) {
 						// Any object returned will be saved in `user` property of the JWT
 						return user
 				} else {
 						// If you return null then an error will be displayed advising the user to check their details.
-						return null
+						return null;
 
 						// You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
 				}
